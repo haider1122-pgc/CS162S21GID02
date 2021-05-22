7f81ccf1f83eb79ebcf7033125f58878f746d995
Nabeel Yousaf <n.y.562448@gmail.com>
2021-05-22 16:06:00 +0500

    GUI 1

--- Carrefour/src/carrefour/Carrefour.java
+++ Carrefour/src/carrefour/Carrefour.java
@@ -16,6 +16,8 @@ public class Carrefour {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        ClientForm cf = new ClientForm();
+        cf.setVisible(true);
     }
     
 }
